2e2f5836fd9e63a4904110bf13d9cfb41cd01d7c
Xavier Moorman <moormanx@educ.college-montreal.qc.ca>
2023-09-12 14:55:59 -0400

    Initial commit

---
+++ word_count.py
@@ -0,0 +1,12 @@
+# Made by Xavier Moorman en 2023
+
+def wordcounter():
+    phrase = str(input("Donnez-moi votre phrase!: "))
+
+    nbr_mots = 0
+    nbr_espaces = 0
+    while phrase != " ":
+        print("Votre phrase contient un seul mot!")
+
+
+wordcounter()
